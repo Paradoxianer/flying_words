feaad786cf647d869198df6ccaa4917092145fe4
paradoxon <blablabla!1865Atlassian>
2023-04-14 17:08:23 +0200

    second try

--- lib/src/play_session/spiral.dart
+++ lib/src/play_session/spiral.dart
@@ -1,11 +1,12 @@
 import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Spiral extends StatefulWidget {
   final String text;
   final double speed;
 
-  Spiral({required this.text, this.speed = 100.0});
+  Spiral({required this.text,  this.speed=0.01});
 
   @override
   _SpiralState createState() => _SpiralState();
@@ -13,44 +14,47 @@ class Spiral extends StatefulWidget {
 
 class _SpiralState extends State<Spiral> with TickerProviderStateMixin {
   late AnimationController _controller;
-  late List<Widget> _textWidgets;
-  late double _radius;
-  late double _angle;
-  late int _currentIndex;
+  late List<Animation<double>> _animations;
+  late List<double> _delays;
 
   late MediaQueryData _mediaQueryData;
+  double _screenWidth = 0.0;
+  double _screenHeight = 0.0;
+
+  List<String> get _words => widget.text.split(' ');
 
   @override
   void initState() {
     super.initState();
-
-    _textWidgets = [];
-    _radius = 20.0;
-    _angle = 0.0;
-    _currentIndex = 0;
-
-    // Calculate the duration based on the speed
-    double distance = _mediaQueryData.size.width / 2 - 50;
-    double durationInSeconds = distance / widget.speed;
-    Duration duration = Duration(seconds: durationInSeconds.toInt());
-
-    // Create a new animation controller with the calculated duration
     _controller = AnimationController(
-      duration: duration,
       vsync: this,
-    );
-
-    // Add a listener to the animation controller to update the UI
-    _controller.addListener(() {
-      setState(() {
-        _angle += 0.05;
-        _radius += 2.0;
-        _currentIndex = (_currentIndex + 1) % widget.text.split(" ").length;
-      });
+      duration: Duration(seconds: 1),
+    )..addListener(() {
+      setState(() {});
     });
 
-    // Start the animation controller
-    _controller.repeat();
+    _mediaQueryData = MediaQuery.of(context);
+    _screenWidth = _mediaQueryData.size.width;
+    _screenHeight = _mediaQueryData.size.height;
+
+    _animations = List.generate(_words.length, (index) {
+      final distance = _screenWidth / 2 - 50 - (index * 10);
+      final durationInSeconds = distance / widget.speed;
+      final duration = Duration(seconds: durationInSeconds.toInt());
+
+      return Tween(begin: 0.0, end: distance).animate(
+        CurvedAnimation(
+          parent: _controller,
+          curve: Interval(_delays[index], 1.0, curve: Curves.easeInOut),
+        ),
+      );
+    });
+
+    _delays = List.generate(_words.length, (index) {
+      return index * 0.5;
+    });
+
+    _controller.forward();
   }
 
   @override
@@ -60,43 +64,31 @@ class _SpiralState extends State<Spiral> with TickerProviderStateMixin {
   }
 
   @override
-  Widget build(BuildContext context) {
+  void didChangeDependencies() {
+    super.didChangeDependencies();
     _mediaQueryData = MediaQuery.of(context);
+  }
 
-    List<String> words = widget.text.split(" ");
-
-    // Generate a list of text widgets with decreasing font sizes
-    _textWidgets = List.generate(
-      words.length,
-          (index) => Text(
-        words[index],
-        style: TextStyle(
-          fontSize: 20.0 - (index * 2),
-          color: Colors.black,
-        ),
-      ),
-    );
-
+  @override
+  Widget build(BuildContext context) {
     return Container(
       child: Stack(
-        children: _textWidgets.map((textWidget) {
-          // Calculate the x and y position of the text widget
-          double x = _radius * cos(_angle);
-          double y = _radius * sin(_angle);
-
-          // Calculate the speed based on the distance
-          double distance = sqrt(pow(x, 2) + pow(y, 2));
-          double speed = distance / (_controller.duration!.inSeconds.toDouble());
-
-          // Increment the angle for the next text widget
-          _angle += (2 * pi) / _textWidgets.length;
-
-          return Positioned(
-            left: (_mediaQueryData.size.width / 2) + x - 25,
-            top: (_mediaQueryData.size.height / 2) + y - 12.5,
-            child: textWidget,
-          );
-        }).toList(),
+        children: [
+          for (var i = 0; i < _words.length; i++)
+            Positioned(
+              left: _screenWidth / 2 +
+                  _animations[i].value * cos(i * pi / 2),
+              top: _screenHeight / 2 -
+                  _animations[i].value * sin(i * pi / 2),
+              child: Transform.scale(
+                scale: 1 + _animations[i].value / _screenWidth,
+                child: Text(
+                  _words[i],
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }

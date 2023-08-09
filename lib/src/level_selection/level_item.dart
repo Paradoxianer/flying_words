dd1bb95e226eb8eac9c7082344fb1581a75b11ef
paradoxon <blablabla!1865Atlassian>
2023-08-09 10:51:09 +0200

    Level Item are now wraped in a Card and Listtile Widget wich enables us to use a "headline" which consists of the verse... later there will be times on the bottom... and we should enable / disable difficutly stages according to the solved difficutly states eg. only enable difficutly.normal when difficutly.easy was solved

--- lib/src/level_selection/level_item.dart
+++ lib/src/level_selection/level_item.dart
@@ -6,42 +6,48 @@ import 'package:go_router/go_router.dart';
 import '../audio/audio_controller.dart';
 import '../audio/sounds.dart';
 
-
 class LevelItem extends StatelessWidget {
   final level;
   LevelItem(this.level);
 
   Widget build(BuildContext context) {
-    return Expanded(
-        child: Stack(
+    return Card(
+      child: ListTile(
+        title: Text(
+            level.verse,
+            textAlign: TextAlign.center
+        ),
+        subtitle:
+         Stack(
           alignment: Alignment.centerLeft,
-      children: <Widget>[
-        Expanded(
-        child: Text(level.text,
-        softWrap: true,
-        style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
-        )),
-        Row(
-            children: Difficulty.values
-                .map<Widget>((e) =>
-                Expanded(
-                  child: Opacity(
-                  opacity: 0.5,
-                  child: IconButton(
-                    icon: difficultyImagePath[e]!,
-                      padding: EdgeInsets.zero,
-                      constraints: BoxConstraints(),
-                onPressed: () {
-                  final audioController = context.read<AudioController>();
-                  audioController.playSfx(SfxType.buttonTap);
-                GoRouter.of(context)
-                    .go('/play/session/${level.number}/${e.name}');
-              }
-            ))))
-                .toList(),
-          ),
-        ],
-        )
+          children: <Widget>[
+            Expanded(
+                child: Text(
+              level.text,
+              softWrap: true,
+           //   style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+            )),
+            Row(
+              children: Difficulty.values
+                  .map<Widget>((e) => Expanded(
+                      child: Opacity(
+                          opacity: 0.5,
+                          child: IconButton(
+                              icon: difficultyImagePath[e]!,
+                              padding: EdgeInsets.zero,
+                              //constraints: BoxConstraints(),
+                              onPressed: () {
+                                final audioController =
+                                    context.read<AudioController>();
+                                audioController.playSfx(SfxType.buttonTap);
+                                GoRouter.of(context)
+                                    .go('/play/session/${level.number}/${e.name}');
+                              }))))
+                  .toList(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

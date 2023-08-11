3d41a31beff9d2a5dfe49e8b81671764922ae15b
paradoxon <blablabla!1865Atlassian>
2023-08-11 14:03:30 +0200

    added some more Lessons

--- lib/src/level_selection/levels.dart
+++ lib/src/level_selection/levels.dart
@@ -21,6 +21,11 @@ final gameLevels = [
     verse: "Johannes 3, 16",
     text:  "Denn also hat Gott die Welt geliebt, daß er seinen eingeborenen Sohn gab, auf daß jeder, der an ihn glaubt, nicht verloren gehe, sondern ewiges Leben habe."
   ),
+  Lesson(number: 3, verse: "1 Korinther 13:4-5", text: "Die Liebe ist langmütig und freundlich, die Liebe eifert nicht, die Liebe treibt nicht Mutwillen, sie bläht sich nicht auf, sie verhält sich nicht ungehörig, sie sucht nicht das Ihre, sie lässt sich nicht erbittern, sie rechnet das Böse nicht zu."),
+  Lesson(number: 4, verse: "1 Thessalonicher 5:16-18", text: "Seid allezeit fröhlich, betet ohne Unterlass, seid dankbar in allen Dingen; denn das ist der Wille Gottes in Christus Jesus für euch."),
+  Lesson(number: 5, verse: "4 Mose 6:24-26", text: "Der HERR segne dich und behüte dich; der HERR lasse sein Angesicht leuchten über dir und sei dir gnädig; der HERR hebe sein Angesicht über dich und gebe dir Frieden."),
+  Lesson(number: 6, verse: "Römer 12:12", text: "Seid fröhlich in Hoffnung, geduldig in Trübsal, beharrlich im Gebet.")
+
  /* Lesson(
     number: 3,
     difficulty: 100,
